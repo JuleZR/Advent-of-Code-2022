d34241af0431c58fae1d3c0c5927c165e503a8f1
Julian <j.roth@kaosnetz.de>
2022-12-01 19:20:27 +0100

    Update day1.py
    - Docstrings added
    - minor Adjustments to variable names

--- day1.py
+++ day1.py
@@ -2,7 +2,14 @@
 
 from itertools import groupby
 
-def calc_luggage():
+def calc_calories() -> list:
+    """Reads day1_input.txt line by line, splits the individual values into
+    a list of lists, sums them up, outputs the highest value and returns the
+    list of all values.
+
+    Returns:
+        list: List of all summed up callory values
+    """
     int_list = []
     with open('day1_input.txt', 'r') as file:
         lines = file.readlines()
@@ -10,19 +17,27 @@ def calc_luggage():
             line = line.strip()
             line = int(line) if line != "" else ''
             int_list.append(line)
-        luggage_list = [list(cont) for ele, cont in groupby(int_list, key=bool) if ele]
-        sum_list = [(sum(l)) for l in luggage_list]
+        callory_list = [list(cont) for ele, cont in groupby(int_list, key=bool) if ele]
+        sum_list = [(sum(l)) for l in callory_list]
         print(max(sum_list))
         return sum_list
 
 def calc_top_three(list_of_value:list):
+    """Sorts a list of calorie values backwards, and prints the sum of the three
+    largest values
+
+    Args:
+        list_of_value (list): List of all summed up callory values
+    """
     sorted_values = sorted(list_of_value, reverse=True)
     top_3 = sum(sorted_values[:3])
     print(top_3)
 
 def main():
-    luggage = calc_luggage()
+    """Main Function"""
+    luggage = calc_calories()
     calc_top_three(luggage)
 
 if __name__ == '__main__':
     main()
+    
