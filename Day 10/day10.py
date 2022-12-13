dda6dc4e949e3495215a864d5425f2e6a52f66bb
Julian <j.roth@kaosnetz.de>
2022-12-13 17:18:27 +0100

    Day 10 Part 2 solved

--- Day 10/day10.py
+++ Day 10/day10.py
@@ -40,6 +40,16 @@ class Process:
             summed += product
         return summed
 
+    def x_for_cycle(self, start: int, end: int):
+        cycle = 0
+        register = 1
+        x_values = []
+        for cmd in self.program:
+            cycle += cmd[0]
+            register += cmd[1]
+            if cycle in range(start, end):
+                x_values.append(register)
+        return x_values
 
 def parse(filename: str):
     try:
@@ -48,19 +58,46 @@ def parse(filename: str):
     except FileNotFoundError:
         print(f"[404]: File {filename} not found")
 
+def draw(x_val):
+    output = ""
+    for i in range(40):
+        rng = x_val[i]
+        if i in range(rng-2, rng+1):
+            output += "#"
+        else:
+            output += "."
+    print(output)
 
-def solution(commands):
-    solve_tbl = [['Advent of Code 2022', 'Day 10']]
+def solution1(commands):
     prog = Process(commands)
     prog.calc_run()
-    print(prog.register)
-    print(prog.sum_list())
+    print('PART I ======================================\n')
+    print(f"Sum of X Values: {prog.sum_list()}\n\n")
 
+
+def solution2(commands):
+    prog2 = Process(commands)
+    prog2.calc_run()
+    xl_1 = prog2.x_for_cycle(0, 41)
+    xl_2 = prog2.x_for_cycle(41, 81)
+    xl_3 = prog2.x_for_cycle(81, 121)
+    xl_4 = prog2.x_for_cycle(121, 161)
+    xl_5 = prog2.x_for_cycle(161, 201)
+    xl_6 = prog2.x_for_cycle(201, 241)
+    print('PART II =====================================\n')
+    draw(xl_1)
+    draw(xl_2)
+    draw(xl_3)
+    draw(xl_4)
+    draw(xl_5)
+    draw(xl_6)
+    
 
 def main():
     """ Main function """
     commands = parse('day10_input.txt')
-    solution(commands)
+    solution1(commands)
+    solution2(commands)
 
 
 if __name__ == '__main__':
