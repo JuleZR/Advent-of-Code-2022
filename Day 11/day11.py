61bb28bcb0cb96717251a27e54d56b5636d47958
Julian <j.roth@kaosnetz.de>
2022-12-15 18:46:10 +0100

    code redesign
    part 2 still bugged

--- Day 11/day11.py
+++ Day 11/day11.py
@@ -1,132 +1,160 @@
 """
 Advent of Code 2022
-Day     11
+Day   11
 """
 
 
-def parse(filename):
-    with open(filename, 'r') as file:
-        lines = [line.strip('\n') for line in file.readlines()]
+class Monkey:
+    def __init__(self, index: int, items: list[int],
+                 operation: list[str, int | str],
+                 divisor: int, test_map: dict):
+        self.index = index
+        self.items = items
+        self.op = operation
+        self.divisor = divisor
+        self.test = test_map
+        self.inspected: int = 0
 
-        # Get Monkeys
-        monkeys = []
-        raw_monkeys = lines[0::7]
-        for m_name in raw_monkeys:
-            m_name = m_name[7:8]
-            monkeys.append(m_name)
-        monkeys = list(map(int, monkeys))
-
-        # Get Startingitems for monkeys
-        starting_items = []
-        raw_items = lines[1::7]
-        for item in raw_items:
-            item = item[18:]
-            item = item.replace(' ', '')
-            item = item.split(',')
-            item = list(map(int, item))
-            starting_items.append(item)
-
-        # Get operation
-        operation = []
-        raw_operation = lines[2::7]
-        for op in raw_operation:
-            op = op[23:]
-            op = op.split(' ')
-            operation.append(op)
-
-        # Get Test
-        tests = []
-        raw_test = lines[3::7]
-        raw_true = lines[4::7]
-        raw_false = lines[5::7]
-        for idx, test in enumerate(raw_test):
-            test = int(test[21:])
-            true_c = raw_true[idx][29:]
-            false_c = raw_false[idx][30:]
-            tests.append([test, {True: int(true_c), False: int(false_c)}])
-
-        # Make Dataset
-        m_dict = {}
-        for i, monkey in enumerate(monkeys):
-            m_dict[monkey] = {
-                'items': starting_items[i],
-                'op': operation[i],
-                'test': tests[i],
-                'inspected': 0
-            }
-    return m_dict
+    def __repr__(self) -> str:
+        return f'#{self.index} 🐒'
 
 
-def calc_worry(item_level: int, operator: str, op_value: str):
-    if op_value == 'old':
-        op_value = item_level
-    match operator:
-        case '+':
-            return item_level + int(op_value)
-        case '-':
-            return item_level - int(op_value)
-        case '*':
-            return item_level * int(op_value)
-        case '/':
-            return item_level / int(op_value)
+class MonkeyHorde:
+    def __init__(self, filename: str):
+        self.monkeys: list = []
+        self._parse(filename)
+        self.mod_product = self._calc_product()
+
+    def _parse(self, filename):
+        with open(filename, 'r') as f:
+            lines = [line.strip() for line in f.readlines()]
+
+            raw_index = lines[0::7]
+            indeces = [int(i[7::].rstrip(':')) for i in raw_index]
+
+            raw_items = lines[1::7]
+            items: list = []
+            for itm in raw_items:
+                itm = itm[16::].replace(' ', '').split(',')
+                itm = list(map(int, itm))
+                items.append(itm)
+
+            raw_op = lines[2::7]
+            op_lst: list = []
+            for op in raw_op:
+                op = op[21::].split(' ')
+                op = [int(o) if o.isdigit() else o for o in op]
+                op_lst.append(op)
+
+            raw_divisor = lines[3::7]
+            divisor_lst: list = []
+            for div in raw_divisor:
+                div = div[19::]
+                divisor_lst.append(div)
+            divisor_lst = list(map(int, divisor_lst))
+
+            test: list = []
+            raw_test_true = lines[4::7]
+            raw_test_false = lines[5::7]
+            for idx, t_test in enumerate(raw_test_true):
+                t_test = int(t_test[25::])
+                f_test = int(raw_test_false[idx][26::])
+                test_dict = {True: t_test, False: f_test}
+                test.append(test_dict)
+
+            for i, m in enumerate(indeces):
+                self.monkeys.append(
+                    Monkey(
+                        indeces[i], items[i], op_lst[i],
+                        divisor_lst[i], test[i])
+                )
+
+    def _calc_worry(self, item_lvl: int, op_lst: list) -> int():
+        op, val = op_lst
+        if val == "old":
+            val = item_lvl
+        match op:
+            case '+':
+                return item_lvl + val
+            case '-':
+                return item_lvl - val
+            case '*':
+                return item_lvl * val
+
+    def _calc_product(self) -> int:
+        product = 1
+        for mon in self.monkeys:
+            product *= mon.divisor
+        return product
+
+    def is_congruent(self, worry: int) -> bool:
+        return worry % self.mod_product == 0
+
+    def is_devisable(self, worry: int, divisor: int) -> bool:
+        return worry % divisor == 0
+
+    def throw_items_1(self):
+        for _ in range(20):
+            for m in self.monkeys:
+                for idx, item in enumerate(m.items):
+                    worry = self._calc_worry(item, m.op)
+                    bored_worry = worry // 3
+                    div_chk = self.is_devisable(bored_worry, m.divisor)
+                    target = m.test[div_chk]
+                    self.monkeys[target].items.append(bored_worry)
+                    m.inspected += 1
+
+                    if idx == len(m.items) - 1:
+                        m.items = []
+
+    def throw_items_2(self):
+        for _ in range(10_000):
+            for m in self.monkeys:
+                for idx, item in enumerate(m.items):
+                    worry = self._calc_worry(item, m.op)
+                    div_chk = self.is_congruent(worry)
+                    target = m.test[div_chk]
+                    self.monkeys[target].items.append(worry)
+                    m.inspected += 1
+
+                    if idx == len(m.items) - 1:
+                        m.items = []
 
 
-def is_devisable_by(c_worry: int, devisor: int):
-    if c_worry % devisor == 0:
-        return True
-    else:
-        return False
-
-
-def product(*args):
+def calc_product(*args):
     product = 1
-    for a in args:
-        product *= a
+    for arg in args:
+        product *= arg
     return product
 
 
-def get_monkey_buissness(inspected: list):
-    inspected.sort(reverse=True)
-    inspected = inspected[0:2]
-    buisness = product(*inspected)
-    print('=== PART 1 ===')
-    print(f"Monkey buissness = {buisness}")
+def part_one(filename: str):
+    part_1 = MonkeyHorde(filename)
+    part_1.throw_items_1()
+
+    buisiness_1: list = []
+    for mon in part_1.monkeys:
+        buisiness_1.append(mon.inspected)
+    buisiness_1.sort(reverse=True)
+    buisiness_1 = buisiness_1[:2]
+    print(calc_product(*buisiness_1))
 
 
-def part_one(monkey_dict: dict):
-    for _ in range(20):
-        for monkey in monkey_dict:
-            for idx, item in enumerate(monkey_dict[monkey]['items']):
-                # calculate worry level
-                op = monkey_dict[monkey]['op'][0]
-                multiplyer = monkey_dict[monkey]['op'][1]
-                worry_level = calc_worry(item, op, multiplyer)
-
-                c_worry = int(worry_level / 3)
-
-                devisor = monkey_dict[monkey]['test'][0]
-                devide_chk = is_devisable_by(c_worry, devisor)
-
-                target_monkey = monkey_dict[monkey]['test'][1][devide_chk]
-                monkey_dict[target_monkey]['items'].append(c_worry)
-
-                monkey_dict[monkey]['inspected'] += 1
-
-                if idx == len(monkey_dict[monkey]['items']) - 1:
-                    monkey_dict[monkey]['items'] = []
-
-    monkey_inspected = []
-    for monkey in monkey_dict:
-        inspect = monkey_dict[monkey]['inspected']
-        monkey_inspected.append(inspect)
-    get_monkey_buissness(monkey_inspected)
+def part_two(filename: str):
+    part_2 = MonkeyHorde(filename)
+    part_2.throw_items_2()
+    buisiness_2:  list = []
+    for mon in part_2.monkeys:
+        buisiness_2.append(mon.inspected)
+    buisiness_2.sort(reverse=True)
+    buisiness_2 = buisiness_2[:2]
+    print(calc_product(*buisiness_2))
 
 
 def main():
-    """ Main function"""
-    p_input = parse('day11_input.txt')
-    part_one(p_input)
+    part_one('day11_input.txt')
+    part_two('example1.txt')
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
